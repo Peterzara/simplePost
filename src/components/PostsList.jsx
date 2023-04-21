dc05154a060cf3ba3ba0ec6464b5ef1f69734304
Peterzara <panrui19980316@gmail.com>
2023-04-21 12:07:38 +0200

    add files

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,13 +1,32 @@
-import Post from './Post'
+import { useState } from "react";
+import Post from "./Post";
 import classes from "./PostsList.module.css";
+import NewPost from "./NewPost";
 
 function PostsList(props) {
-    return (
+  const [enteredBody, setBody] = useState("");
+  const [enteredAuthor, setAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      <NewPost
+        onBodyChange={changeBodyHandler}
+        onAuthorChange={changeAuthorHandler}
+      />
       <ul className={classes.posts}>
-        <Post theme="Coding is cool" author="Ray" />
+        <Post theme={enteredBody} author={enteredAuthor} />
         <Post theme="Engineering is awesome" author="Ray" />
       </ul>
-    );
+    </>
+  );
 }
 
 export default PostsList;
