6bfa86576b4cbe6f5aaf62a79178ee13bda87cbe
Peterzara <panrui19980316@gmail.com>
2023-04-24 11:06:15 +0200

    use useEffect hook to fetch data from backend without causing infinite loop in component

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,36 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Post from "./Post";
 import classes from "./PostsList.module.css";
 import NewPost from "./NewPost";
 import Modal from "./Modal";
 
 function PostsList({ isPosting, onStopPosting }) {
-  const [enteredBody, setBody] = useState("");
-  const [enteredAuthor, setAuthor] = useState("");
+  const [posts, setPosts] = useState([]);
 
-  function changeBodyHandler(event) {
-    setBody(event.target.value);
-  }
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
 
-  function changeAuthorHandler(event) {
-    setAuthor(event.target.value);
+    fetchPosts();
+  }, []);
+
+  function addPostHandler(newPost) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(newPost),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    setPosts((existingPosts) => [newPost, ...existingPosts]);
   }
 
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={changeBodyHandler}
-            onAuthorChange={changeAuthorHandler}
-          />
+          <NewPost onCancel={onStopPosting} onAddNewPost={addPostHandler} />
         </Modal>
       )}
 
-      <ul className={classes.posts}>
-        <Post theme={enteredBody} author={enteredAuthor} />
-        <Post theme="Engineering is awesome" author="Ray" />
-      </ul>
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => {
+            return (
+              <Post key={post.body} theme={post.body} author={post.author} />
+            );
+          })}
+        </ul>
+      )}
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There is no posts yet</h2>
+        </div>
+      )}
     </>
   );
 }
