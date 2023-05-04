da0029f73f99e5177ea3b48ba6a4f3f95e315036
Peterzara <panrui19980316@gmail.com>
2023-05-04 23:05:08 +0800

    refactor newPost form submission with action

--- src/routes/NewPost.jsx
+++ src/routes/NewPost.jsx
@@ -1,45 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, Form, redirect } from "react-router-dom";
 import Modal from "../components/Modal";
 import classes from "./NewPost.module.css";
 import { useState } from "react";
 
 function NewPost({ onCancel, onAddNewPost }) {
-  const [enteredBody, setBody] = useState("");
-  const [enteredAuthor, setAuthor] = useState("");
-
-  function changeBodyHandler(event) {
-    setBody(event.target.value);
-  }
-
-  function changeAuthorHandler(event) {
-    setAuthor(event.target.value);
-  }
-
-  function submitHandler(event) {
-    // event.preventDefault();
-    const newPost = {
-      body: enteredBody,
-      author: enteredAuthor,
-    };
-    onAddNewPost(newPost);
-    onCancel();
-  }
-
   return (
     <Modal>
-      <form className={classes.form} onSubmit={submitHandler}>
+      <Form method="post" className={classes.form}>
         <p>
           <label htmlFor="body">Text</label>
-          <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+          <textarea id="body" required rows={3} name="body" />
         </p>
         <p>
           <label htmlFor="name">Your name</label>
-          <input
-            type="text"
-            id="name"
-            required
-            onChange={changeAuthorHandler}
-          />
+          <input type="text" id="name" required name="author" />
         </p>
         <p className={classes.actions}>
           <button>Submit</button>
@@ -47,9 +21,24 @@ function NewPost({ onCancel, onAddNewPost }) {
             Cancel
           </Link>
         </p>
-      </form>
+      </Form>
     </Modal>
   );
 }
 
 export default NewPost;
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const postData = await Object.fromEntries(formData);
+
+  await fetch("http://localhost:8080/posts", {
+    method: "POST",
+    body: JSON.stringify(postData),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  return redirect("/");
+}
